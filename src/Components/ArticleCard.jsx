93c1e0e97fd9eb8fc14c67b54ab2b1a0f0fded9a
Patrick Mackridge <pat.mack@talktalk.net>
2020-03-18 09:07:53 +0000

    Article card now shows date formatted with moment.js

--- src/Components/ArticleCard.jsx
+++ src/Components/ArticleCard.jsx
@@ -1,24 +1,24 @@
 import React from "react";
 import { Link } from "@reach/router";
+import * as moment from "moment";
 
 const ArticleCard = ({ article }) => {
   return (
     <div className="article-card">
       <Link to={`/${article.topic}`}>
-        <div id="card-topic">{article.topic}</div>{" "}
+        <div id="card-topic">
+          {article.topic.slice(0, 1).toUpperCase() + article.topic.slice(1)}
+        </div>
       </Link>
       <div id="card-author">{article.author}</div>
       <div id="card-title">{article.title}</div>
       <div id="card-date">
-        {article.created_at.slice(0, 10)} at {article.created_at.slice(-13, -5)}
+        Posted: {moment(article.created_at).format("Do MMM YYYY, h:mm:ss a")}
       </div>
       <div id="card-comments">{article.comment_count} Comments</div>
       <div id="card-votes">Votes: {article.votes}</div>
     </div>
   );
 };
-
-// moment relative to now
-// javascript date methods for exact dates
 
 export default ArticleCard;
